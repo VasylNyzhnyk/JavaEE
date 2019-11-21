6d291d80b98106a31e2176494e37ce38908833ea
My <vasylnv19@gmail.com>
2019-11-21 15:02:50 +0200

    rename

--- HomWork/BigBenClock/Testovyi.java
+++ HomWork/BigBenClock/Testovyi.java
@@ -1,12 +1,6 @@
 package com.company.HomWork.BigBenClock;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-
-/**
- * Created by Pc on 19.01.2017.
- *//*менше число*/
+/*менше число*/
 public class Testovyi {
     public static void main(String[] args) throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
