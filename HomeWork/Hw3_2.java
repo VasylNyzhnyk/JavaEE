c9990a62467b2c16d9c735b55ae352077e0be1a0
My <vasylnv19@gmail.com>
2019-11-27 18:00:32 +0200

    forEach

---
+++ HomeWork/Hw3_2.java
@@ -0,0 +1,31 @@
+package com.company.HomWork;
+
+/* Рисуем линии
+Используя цикл for вывести на экран:
+- горизонтальную линию из 10 восьмёрок
+- вертикальную линию из 10 восьмёрок.
+8888888888
+8
+8
+8
+8
+8
+8
+8
+8
+8
+8
+*/
+public class Hw3_2 {
+    public static void main(String[] args) {
+
+            for(int j = 0; j < 10; j++){
+                System.out.print(8);
+            }
+        System.out.println();
+            for(int i = 0; i <10; i++)
+                System.out.println(8);
+
+
+    }
+}
